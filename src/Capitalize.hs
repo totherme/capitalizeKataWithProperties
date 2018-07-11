edbdd4a6702a36c1504b2531411d43d43b65b3f2
Gareth Smith <gsmith@pivotal.io>
2018-07-11 08:43:51 +0100

    Lint everything

--- src/Capitalize.hs
+++ src/Capitalize.hs
@@ -7,17 +7,17 @@ module Capitalize
 import Data.Char (toUpper, isAlpha)
 
 capitalize :: String -> String
-capitalize = unwordsAndSpaces . map (fmap capitalizeWord) . wordsAndSpaces where
+capitalize = unwordsAndSpaces . map (fmap capitalizeWord) . wordsAndSpaces
 
 capitalizeWord :: String -> String
 capitalizeWord "" = ""
-capitalizeWord (x:xs) = (toUpper x) : xs
+capitalizeWord (x:xs) = toUpper x : xs
 
 type SpacesAndWords = (String, String)
 
 wordsAndSpaces :: String -> [SpacesAndWords]
 wordsAndSpaces "" = []
-wordsAndSpaces s = (firstSpaces, firstWord) : (wordsAndSpaces rest) where
+wordsAndSpaces s = (firstSpaces, firstWord) : wordsAndSpaces rest where
   firstWord = takeWhile isAlpha s
   firstSpaces = takeWhile (not . isAlpha) $ dropWhile isAlpha s
   rest = dropWhile (not . isAlpha) $ dropWhile isAlpha s
@@ -26,4 +26,4 @@ unwordsAndSpaces :: [SpacesAndWords] -> String
 unwordsAndSpaces [] = ""
 unwordsAndSpaces ((spaces, word):rest) = word ++
                                          spaces ++
-                                         (unwordsAndSpaces rest)
+                                         unwordsAndSpaces rest
